df31a0dd2c3f5f4cb5ad95273d5cc374d7700774
yaluvv <yaluvv@yandex.ru>
2022-10-19 14:22:05 +0300

    fix audio url

--- src/modules/playlist.js
+++ src/modules/playlist.js
@@ -1,12 +1,12 @@
 const playList = [
     {
         title: 'Aqua Caelestis',
-        src: 'https://yaluvv.github.io/moment-app/src/sounds/Aqua%20Caelestis.mp3',
+        src: 'https://yaluvv.github.io/momentum-app/src/sounds/Aqua%20Caelestis.mp3',
         duration: '00:58'
     },
     {
         title: 'River Flows In You',
-        src: 'https://yaluvv.github.io/moment-app/src/sounds/River%20Flows%20In%20You.mp3',
+        src: 'https://yaluvv.github.io/momentum-app/src/sounds/River%20Flows%20In%20You.mp3',
         duration: '03:50'
     }
 ]
